c93a6adc04a01d026918a365f16f2c1d61e96ffb
nadarihem <messoussa.rihem1479@yahoo.com>
2023-11-22 19:49:14 +0100

    Ajouter les fichiers (assets, ArtMus.php, index.html, leboncoin.php, mediaqueries.css, qwirkle.php, script.js, style.css)

---
+++ ArtMus.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qwirkle</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header class="menu">
+    <nav id="desktop-nav">
+        <div class="logo">Rihem.</div>
+    </nav>
+    <nav class="navigation"></nav>
+</header>
+
+<section id="profile">
+    <div class="section__pic-container">
+        <img src="./assets/cc.jpg" alt="Nada Rihem profile picture" />
+    </div>
+    <div class="section__text">
+        <h1 class="title">Description</h1>
+        <p class="section__text__p2"> 
+            ArtMus Virtual Exhibition redefines art access through an immersive web application offering virtual museum tours.
+             Explore galleries, zoom into artworks, and access detailed information from the comfort of your home. 
+             An interactive, informative experience accessible to all, 
+             expanding the reach of art beyond the physical confines of museums.</p>
+    </div>
+</section>
+
+<section id="hobbies">
+    <p class="section__text__p1"></p>
+    <h1 class="title">project</h1>
+    <div class="experience-details-container">
+        <div class="about-containers">
+            <div class="details-container color-container">
+                <div class="article-container">
+                    <img
+                        src="./assets/ccc2.jpg"
+                        alt="Project 1"
+                        class="project-img"
+                    />
+                </div>
+               
+                <div class="btn-container"></div>
+            </div>
+            <div class="details-container color-container">
+                <div class="article-container">
+                    <img
+                        src="./assets/ccc4.jpg"
+                        alt="Project 2"
+                        class="project-img"
+                    />
+                </div>
+               
+                <div class="btn-container"></div>
+            </div>
+            <div class="details-container color-container">
+                <div class="article-container">
+                    <img
+                        src="./assets/ccc3.jpg"
+                        alt="Project 3"
+                        class="project-img"
+                    />
+                </div>
+                
+                <div class="btn-container"></div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
---
+++ leboncoin.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qwirkle</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header class="menu">
+    <nav id="desktop-nav">
+        <div class="logo">Rihem.</div>
+    </nav>
+    <nav class="navigation"></nav>
+</header>
+
+<section id="profile">
+    <div class="section__pic-container">
+        <img src="./assets/project-1.png" alt="Nada Rihem profile picture" />
+    </div>
+    <div class="section__text">
+        <h1 class="title">Description</h1>
+        <p class="section__text__p2"> A resale website created by drawing inspiration from the real Leboncoin website, including the database and all existing features (Messaging, Posting ads, Favorites, Secure Registration and Login with cryptographic hashing system, Filtering, Modifying, and Deleting ads).
+
+(Languages used: PHP, HTML, CSS).</p>
+    </div>
+</section>
+
+<section id="hobbies">
+    <p class="section__text__p1"></p>
+    <h1 class="title">project</h1>
+    <div class="experience-details-container">
+        <div class="about-containers">
+            <div class="details-container color-container">
+                <div class="article-container">
+                    <img
+                        src="./assets/c1.png"
+                        alt="Project 1"
+                        class="project-img"
+                    />
+                </div>
+               
+                <div class="btn-container"></div>
+            </div>
+            <div class="details-container color-container">
+                <div class="article-container">
+                    <img
+                        src="./assets/c4.png"
+                        alt="Project 2"
+                        class="project-img"
+                    />
+                </div>
+               
+                <div class="btn-container"></div>
+            </div>
+            <div class="details-container color-container">
+                <div class="article-container">
+                    <img
+                        src="./assets/c3.png"
+                        alt="Project 3"
+                        class="project-img"
+                    />
+                </div>
+                
+                <div class="btn-container"></div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
