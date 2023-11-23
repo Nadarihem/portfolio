077238fa8c6c1c0b3aa3564d1b36035beb9c9dfd
nadarihem <messoussa.rihem1479@yahoo.com>
2023-11-23 15:02:09 +0100

    Ajouter les fichiers (assets, ArtMus.php, index.html, leboncoin.php, mediaqueries.css, qwirkle.php, script.js, style.css)

--- qwirkle.php
+++ qwirkle.php
@@ -1,74 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Qwirkle</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
-<header class="menu">
-    <nav id="desktop-nav">
-        <div class="logo">Rihem.</div>
-    </nav>
-    <nav class="navigation"></nav>
-</header>
+    <header class="menu">
+        <nav id="desktop-nav">
+            <div class="logo">Rihem.</div>
+        </nav>
+        <nav class="navigation"></nav>
+    </header>
 
-<section id="profile">
-    <div class="section__pic-container">
-        <img src="./assets/project-2.png" alt="Nada Rihem profile picture" />
-    </div>
-    <div class="section__text">
-        <h1 class="title">Description</h1>
-        <p class="section__text__p2">Qwirkle is a board game that consists of 108 wooden tiles with 6 shapes in 6 different colors. Each player tries to score the maximum number of points by forming lines of tiles that share a common color or shape.
-
-(Language used: C).</p>
-    </div>
-</section>
-
-<section id="hobbies">
-    <p class="section__text__p1"></p>
-    <h1 class="title">project</h1>
-    <div class="experience-details-container">
-        <div class="about-containers">
-            <div class="details-container color-container">
-                <div class="article-container">
-                    <img
-                        src="./assets/cc6.png"
-                        alt="Project 1"
-                        class="project-img"
-                    />
-                </div>
-               
-                <div class="btn-container"></div>
-            </div>
-            <div class="details-container color-container">
-                <div class="article-container">
-                    <img
-                        src="./assets/cc5.png"
-                        alt="Project 2"
-                        class="project-img"
-                    />
-                </div>
-               
-                <div class="btn-container"></div>
-            </div>
-            <div class="details-container color-container">
-                <div class="article-container">
-                    <img
-                        src="./assets/cc2.png"
-                        alt="Project 3"
-                        class="project-img"
-                    />
-                </div>
-                
-                <div class="btn-container"></div>
-            </div>
-            
+    <section id="profile">
+        <div class="section__pic-container">
+            <img src="./assets/project-2.png" alt="Nada Rihem profile picture" />
         </div>
-    </div>
-</section>
+        <div class="section__text">
+            <h1 class="title">Description</h1>
+            <p class="section__text__p2">Qwirkle is a board game that consists of 108 wooden tiles with 6 shapes in 6 different colors. Each player tries to score the maximum number of points by forming lines of tiles that share a common color or shape. (Language used: C).</p>
+        </div>
+    </section>
+
+    <section id="hobbies">
+        <p class="section__text__p1"></p>
+        <h1 class="title">project</h1>
+        <div class="experience-details-container">
+            <div class="about-containers">
+                <div class="details-container color-container">
+                    <div class="article-container">
+                        <img
+                            src="./assets/cc6.png"
+                            alt="Project 1"
+                            class="project-img"
+                        />
+                    </div>
+
+                    <div class="btn-container">
+                        <a href="qwirkle.php" style="text-decoration: none; text-align: center;">
+                            <button style="background-color: #000; color: #fff; border-radius: 2px;">Project 1</button>
+                        </a>
+                    </div>
+                </div>
+                <div class="details-container color-container">
+                    <div class="article-container">
+                        <img
+                            src="./assets/cc5.png"
+                            alt="Project 2"
+                            class="project-img"
+                        />
+                    </div>
+
+                    <div class="btn-container">
+                        <a href="qwirkle.php" style="text-decoration: none; text-align: center;">
+                            <button style="background-color: #000; color: #fff; border-radius: 2px;">Project 2</button>
+                        </a>
+                    </div>
+                </div>
+                <div class="details-container color-container">
+                    <div class="article-container">
+                        <img
+                            src="./assets/cc2.png"
+                            alt="Project 3"
+                            class="project-img"
+                        />
+                    </div>
+
+                    <div class="btn-container">
+                        <a href="qwirkle.php" style="text-decoration: none; text-align: center;">
+                            <button style="background-color: #000; color: #fff; border-radius: 2px;">Project 3</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 </body>
+
 </html>
